9c8acee17ceacb0caebf7c3d901b8da6ed9c8c4c
Carlos M <carlos.mendez.marquez@gmail.com>
2019-02-15 17:56:35 -0400

    Agregadas las pantallas

--- steps.php
+++ steps.php
@@ -6,13 +6,13 @@
 <div class="tab">
   <div class="wizard-step">
 		<div class="row">
-			<div class="col-md-12 text-center">
+			<div class="col-md-12 text-center questions">
 				<span class="title1">¿TRABAJAS EN UNA<br><b>FARMACIA</b>?</span>
 			</div>
 		</div>
 		<br><br>
 		<div class="row answer-container text-center" >
-			<button type="button" class="button" >NO</button>
+			<button type="button" class="button" onclick="nextPrev(1)">NO</button>
     		<button type="button" class="button" onclick="nextPrev(1)">SI</button>
 
 		</div>
@@ -28,8 +28,8 @@
 		</div>
 		<div class="row answer-container">
 			<div class="col-md-12 text-center">
-				<a class="button" >SI</a>
-				<a class="button" >NO</a>
+				<a class="button" onclick="nextPrev(1)">SI</a>
+				<a class="button" onclick="nextPrev(1)">NO</a>
 			</div>
 		</div>
 	</div>
@@ -107,16 +107,95 @@
 		</div>
 	</div>
 </div>
-
+<div class="tab">
+	<div class="wizard-step">
+		<div class="row">
+			<div class="col-md-12 text-center questions">
+				<span class="title1"><b>¡FELICIDADES!</b><br>¡RESPUESTA CORRECTA!</span>
+			</div>
+		</div>
+		<div class="row answer-container">
+			<p>EN 1989 MARTIDERM LANZA LA<br>PRIMERA AMPOLLA DE VITAMINA<br>C Y PROTEOGLICANO CON EL<br>NOMBRE DE 'PROTEOGLICANOS'</p>
+		</div>
+	</div>
+</div>
+<div class="tab">
+	<div class="wizard-step">
+		<div class="row">
+			<div class="col-md-12 text-center questions">
+				<span class="title1"><b>Oooh...</b><br>¡RESPUESTA CORRECTA!</span>
+			</div>
+		</div>
+		<div class="row answer-container">
+			<p>EN 1989 MARTIDERM LANZA LA<br>PRIMERA AMPOLLA DE VITAMINA<br>C Y PROTEOGLICANO CON EL<br>NOMBRE DE 'PROTEOGLICANOS'</p>
+		</div>
+	</div>
+</div>
+<div class="tab">
+	<div class="wizard-step">
+		<div class="row">
+			<div class="col-md-12 text-center questions">
+				<span class="title1">RELACIONA EL PRODUCTO CON SU DESCRIPCIÓN</span>
+			</div>
+		</div>
+		<div class="row answer-container">
+		</div>
+	</div>
+</div>
+<div class="tab">
+	<div class="wizard-step">
+		<div class="row">
+			<div class="col-md-12 text-center questions">
+				<span class="title1">¿CUÁL DE ESTOS CANALES<br>USAS MÁS PARA<br><b>LLEGAR A TUS CLIENTES?</b></span>
+			</div>
+		</div>
+		<div class="row answer-container">
+			<input type="radio" name="pregunta_7" value="rrss"> RRSS&nbsp;&nbsp;&nbsp;&nbsp;
+  				<input type="radio" name="pregunta_7" value="whatsapp"> WHATSAPP&nbsp;&nbsp;&nbsp;&nbsp;
+  				<input type="radio" name="pregunta_7" value="medios"> MEDIOS LOCALES&nbsp;&nbsp;&nbsp;&nbsp;
+  				<input type="radio" name="pregunta_7" value="boca"> BOCA OREJA&nbsp;&nbsp;&nbsp;&nbsp;
+		</div>
+	</div>
+</div>
+<div class="tab">
+	<div class="wizard-step">
+		<div class="row">
+			<div class="col-md-12 text-center questions">
+				<span class="title1">RECOMENDARÍAS <br><b>MARTIDERM</b><br>A TUS CLIENTES?</span>
+			</div>
+		</div>
+		<div class="row answer-container">
+			<div class="col-md-12 text-center">
+				<a class="button" onclick="nextPrev(1)">SI</a>
+				<a class="button" onclick="nextPrev(1)">NO</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="tab">
+	<div class="wizard-step">
+		<div class="row">
+			<div class="col-md-12 text-center header-text">
+				<span class="title2">¡GRACIAS!</span>
+				<br>
+				<span class="title1">HAS CONSEGUIDO TU<br>KIT DE MUESTRAS</span>
+			</div>
+		</div>
+	</div>
+</div>
 
 <div style="overflow:auto;">
     <div style="float:right;">
-      <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
-      <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+      <button style="display: none;" type="button" id="nextBtn" onclick="nextPrev(1)">Siguiente</button>
     </div>
   </div>
   <!-- Circles which indicates the steps of the form: -->
   <div style="text-align:center;margin-top:40px;">
+    <span class="step"></span>
+    <span class="step"></span>
+    <span class="step"></span>
+    <span class="step"></span>
+    <span class="step"></span>
     <span class="step"></span>
     <span class="step"></span>
     <span class="step"></span>
