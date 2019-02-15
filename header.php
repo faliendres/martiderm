fbd7834c4948d5b1cb3af1e99c41420317ff7650
faliendres <freddy.aliendres@gmail.com>
2019-02-15 14:13:19 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MartiDerm | La ƒórmula</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="assets/css/style.css"> 
+</head>
+<body>
+	<div class="site-content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12 logo">
+					<img src="assets/img/logo.jpg">
+				</div>
+			</div>
+			<div class="content-spacing">
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+	include 'header.php';
+?>
+
+	<div class="row">
+		<div class="col-md-12 text-center header-text">
+			<span class="title1">DESCUBRE LA FÓRMULA</span>
+			<br>
+			<span class="title2">MARTIDERM</span>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 text-center link">
+			<h2><b><a href="step1.php">HAZ CLICK AQUÍ</a></b></h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<img style="box-shadow: inset 0px 0px 40px 40px #f7f7f7;" src="assets/img/quizz-app-1.jpg">	
+		</div>
+	</div>
+
+<?php
+	include 'footer.php';
+?>	
