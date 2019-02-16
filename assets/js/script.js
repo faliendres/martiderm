88092cbe5d8414c9f3755b50e5cae68e44e8cbfa
Carlos M <carlos.mendez.marquez@gmail.com>
2019-02-15 21:00:45 -0400

    Agregada funcionalidad a botones de pregunta 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,3 +84,15 @@ jQuery(".p_3").on("click", function () {
       nextPrev(1);
     }
 });
+
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn-opt");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
