45ce7bb86e070014f66d8b72c90a60109261377a
Evelyn Ruiz <evelynruiz15@gmail.com>
2019-02-16 23:15:03 -0400

    correccion db

--- db.php
+++ db.php
@@ -13,7 +13,9 @@ if(isset($_POST['quiz'])){
 	    die("Connection failed: " . $conn->connect_error);
 	} 
 
-	$sql = "INSERT INTO 'quiz' ('trabaja_farmacia', 'conocias_martiderm', 'tipos_ampollas', 'afirmaciones_martiderm', 'primer_laboratorio', 'relacion_producto_descripcion', 'canal_comunicacion', 'recomiendas_martiderm', 'fecha_creacion') VALUES ($_POST['pregunta_1'], $_POST['pregunta_2'], $_POST['pregunta_3'], $_POST['pregunta_4'], $_POST['pregunta_5'], $_POST['pregunta_6'], $_POST['pregunta_7'], $_POST['pregunta_8'], CURRENT_TIMESTAMP);";
+	$sql = "INSERT INTO 'quiz' ('trabaja_farmacia', 'conocias_martiderm', 'tipos_ampollas', 'afirmaciones_martiderm', 'primer_laboratorio', 'relacion_producto_descripcion', 'canal_comunicacion', 'recomiendas_martiderm', 'fecha_creacion') VALUES (".$_POST['pregunta_1'].", ".$_POST['pregunta_2'].", ".$_POST['pregunta_3'].", ".$_POST['pregunta_4'].", ".$_POST['pregunta_5'].", ".$_POST['pregunta_6'].", ".$_POST['pregunta_7'].", ".$_POST['pregunta_8'].", CURRENT_TIMESTAMP)";
+
+	echo $sql;
 
 	if ($conn->query($sql) === TRUE) {
 	    echo "New record created successfully";
